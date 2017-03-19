faa8244984c26d0f6136d14c75b86cca5497c71a
huaxueyihao <837403116@qq.com>
2017-03-19 21:01:17 +0800

    spring-in-action chapter01 commit

---
+++ springinaction/chapter01/src/main/java/com/springinaction/springidol/Stage.java
@@ -0,0 +1,19 @@
+package com.springinaction.springidol;
+
+/**
+ * µ¥ÀıÀà£ºÎèÌ¨
+ *
+ */
+public class Stage {
+	private Stage(){
+		
+	}
+	
+	private static class StageSingletonHolder{
+		static Stage instance = new Stage();
+	}
+	
+	public static Stage getInstance() {
+		return StageSingletonHolder.instance;
+	}
+}

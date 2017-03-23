b35f494384cbcfbd4c335ee08982c5b2f1060544
huaxueyihao <837403116@qq.com>
2017-03-23 20:09:13 +0800

    springinaction commit chapter04

---
+++ springinaction/chapter04/src/main/java/com/springinaction/springidol/Saxophone.java
@@ -0,0 +1,16 @@
+package com.springinaction.springidol;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Saxophone implements Instrument {
+	
+	public Saxophone(){
+		
+	}
+	
+	public void play() {
+		System.out.println("TOOT¡¡TOOT TOOT");
+	}
+
+}
